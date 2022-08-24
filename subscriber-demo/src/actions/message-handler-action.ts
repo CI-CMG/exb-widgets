6096ff4b2341c5cf66295a5f8510c43a9ee6fa64
jcc <john.c.cartwright@comcast.net>
2022-08-24 13:55:43 -0600

    experiments with class-type widget

--- subscriber-demo/src/actions/message-handler-action.ts
+++ subscriber-demo/src/actions/message-handler-action.ts
@@ -4,18 +4,21 @@ import {
   Message,
   getAppStore,
   appActions,
-  MessageDescription
+  MessageDescription,
+  ExtentChangeMessage,
+  DataSourceFilterChangeMessage
 } from 'jimu-core'
+import Extent from 'esri/geometry/Extent'
 
 export default class MessageHandlerAction extends AbstractMessageAction {
   // new in v1.9, replaces filterMessageDescription. used in builder
   filterMessageDescription (messageDescription: MessageDescription): boolean {
-    console.log('MessageHandlerAction: inside filterMessageDescription with ', messageDescription)
+    // console.log('MessageHandlerAction: inside filterMessageDescription with ', messageDescription)
     return true
   }
 
   filterMessage (message: Message): boolean {
-    console.log('MessageHandlerAction: inside filterMessage with ', message)
+    // console.log('MessageHandlerAction: inside filterMessage with ', message)
 
     // any Message type
     return true
@@ -23,26 +26,32 @@ export default class MessageHandlerAction extends AbstractMessageAction {
 
   //set action setting uri
   getSettingComponentUri (messageType: MessageType, messageWidgetId?: string): string {
-    return 'actions/message-handle-raction-setting'
+    return 'actions/message-handler-action-setting'
   }
 
   onExecute (message: Message, actionConfig?: any): Promise<boolean> | boolean {
     switch (message.type) {
       case MessageType.DataSourceFilterChange:
         console.log('MessageHandlerAction: got DataSourceFilterChangeMessage', message, actionConfig)
-
-        // trigger an update for the widget
-        getAppStore().dispatch(appActions.widgetStatePropChange(this.widgetId, 'lastMessage', message.type))
+        const dataSourceFilterChangeMessage = message as DataSourceFilterChangeMessage
+        // TODO since the contents of the DataSourceFilterChangeMessage don't change with the change in queryParams
+        // the widget re-render is never triggered
+        getAppStore().dispatch(appActions.widgetStatePropChange(this.widgetId, 'dataSourceId', dataSourceFilterChangeMessage.dataSourceId))
         break
 
       case MessageType.ExtentChange:
         console.log('MessageHandlerAction: got ExtentChangeMessage', message, actionConfig)
-
-        //trigger an update for the widget - not working!
-        getAppStore().dispatch(appActions.widgetStatePropChange(this.widgetId, 'lastMessage', message.type))
+        const extentChangeMessage = message as ExtentChangeMessage
+        // trigger an update for the widget when Extent is different from previous. Must be a String?
+        getAppStore().dispatch(appActions.widgetStatePropChange(this.widgetId, 'extent', this.formatExtent(extentChangeMessage.extent)))
         break
     }
 
     return true
+  }
+
+  formatExtent (extent: Extent): string {
+    if (!extent) { return '' }
+    return `${extent.xmin}, ${extent.ymin}, ${extent.xmax}, ${extent.ymax}`
   }
 }
--- subscriber-demo/src/runtime/widget.tsx
+++ subscriber-demo/src/runtime/widget.tsx
@@ -1,149 +1,36 @@
-/** @jsx jsx */
 import {
-  AllWidgetProps,
-  jsx,
-  DataSourceComponent,
-  QueriableDataSource
+  React,
+  utils,
+  AllWidgetProps
 } from 'jimu-core'
-import { JimuMapView, JimuMapViewComponent } from 'jimu-arcgis'
-// import defaultMessages from './translations/default'
-// import { defaultMessages as jimuUIMessages } from 'jimu-ui'
-import reactiveUtils from 'esri/core/reactiveUtils'
-import { useState, useEffect } from 'react'
-import { IMConfig } from '../config'
 
-export default function SubscriberDemo (props: AllWidgetProps<IMConfig>) {
-  const [dataSource, setDataSource] = useState(null)
-  const [view, setView] = useState(null)
+export default class Widget extends React.PureComponent<AllWidgetProps<unknown>, { extent: string, dataSourceId: string }> {
+  state = { extent: null, dataSourceId: null }
 
-  // const [lastMessage, setLastMessage] = useState<string>('')
-  const lastMessage = props.stateProps?.lastMessage
-  console.log('rendering SubscriberDemo. props: ', props)
-
-  const featureServiceUrl = props.config.featureServiceUrl ? props.config.featureServiceUrl : 'feature service URL not set'
-  // console.log('featureServiceUrl: ', featureServiceUrl)
-  // console.log('mapView: ', view)
-  // console.log('dataSource: ', dataSource)
-
-  // TODO how should these be typed?
-  let extentWatchHandle
-  let stationaryWatchHandle
-  let queryParamsWatchHandle
-
-  // TODO why is this not working?
-  // reactiveUtils.watch(
-  //   () => props.stateProps?.lastMessage,
-  //   (lastMessage) => {
-  //     console.log('lastMessage updated: ', lastMessage)
-  //   })
-
-  // runs once
-  function onDataSourceCreated (ds: QueriableDataSource) {
-    if (!ds) {
-      console.error('unable to create DataSource')
-      return
-    }
-    console.log('DataSource: ', ds)
-    ds.ready().then(() => {
-      console.log('configQueryParams: ', ds.getConfigQueryParams())
-      console.log('currentQueryParams: ', ds.getCurrentQueryParams())
-      console.log('runtimeQueryParams: ', ds.getRuntimeQueryParams())
-      // if (!queryParamsWatchHandle) {
-      //   queryParamsWatchHandle = reactiveUtils.watch(
-      //     ds.getCurrentQueryParams,
-      //     (oldValue, newValue) => {
-      //       console.log(oldValue, newValue)
-      //     })
-      // }
-    })
-
-    setDataSource(ds)
-  }
-
-  // runs once
-  const activeViewChangeHandler = (jmv: JimuMapView) => {
-    if (!jmv) {
-      console.warn('no MapView')
-      return
-    }
-    setView(jmv.view)
-
-    if (!extentWatchHandle) {
-      extentWatchHandle = jmv.view.watch('extent', (newValue, oldValue) => {
-        //TODO why does this happen?
-        if (newValue.equals(oldValue)) {
-          console.warn('new extent same as old extent')
-          // new extent same as old extent, no action taken
-          return
-        }
-        // TODO trigger some action
-        console.log('Extent changed')
-      })
+  componentDidUpdate (prevProps: AllWidgetProps<unknown>) {
+    console.log('inside componentDidUpdate...')
+    if (utils.getValue(this.props, 'stateProps.extent') !== utils.getValue(prevProps, 'stateProps.extent')) {
+      const extent = utils.getValue(this.props, 'stateProps.extent')
+      console.log(extent)
+      this.setState({ extent: extent })
     }
 
-    // alternative to watching DataSource queryParams property?
-    if (!queryParamsWatchHandle) {
-      const layer = jmv.view.map.layers.find(lyr => lyr.title === 'Deep Sea Coral and Sponge Observations')
-      queryParamsWatchHandle = layer.watch('definitionExpression', (newExpression, oldExpression) => {
-        // TODO trigger some action
-        console.log('layerDefinition changed')
-      })
+    if (utils.getValue(this.props, 'stateProps.dataSourceId') !== utils.getValue(prevProps, 'stateProps.dataSourceId')) {
+      const dataSourceId = utils.getValue(this.props, 'stateProps.dataSourceId')
+      console.log(dataSourceId)
+      this.setState({ dataSourceId: dataSourceId })
     }
   }
 
-  // fires only once, when widget initially opened
-  useEffect(() => {
-    // one-time cleanup function
-    return function cleanup () {
-      // remove at time componment is destroyed
-      if (extentWatchHandle) {
-        extentWatchHandle.remove()
-      }
-      if (stationaryWatchHandle) {
-        stationaryWatchHandle.remove()
-      }
-      if (queryParamsWatchHandle) {
-        stationaryWatchHandle.remove()
-      }
-    }
-  }, [])
+  componentDidMount () {
+    console.log('inside componentDidMount with ', this.props.stateProps?.extent)
+  }
 
-  return (
-    <div className="widget-demo jimu-widget m-2">
-      <DataSourceComponent
-        useDataSource={props.useDataSources?.[0]}
-        widgetId={props.id}
-        onDataSourceCreated={onDataSourceCreated}
-      />
-      <JimuMapViewComponent
-        useMapWidgetId={props.useMapWidgetIds?.[0]}
-        onActiveViewChange={activeViewChangeHandler}
-      />
-      {/* <p>isUpdating: {isUpdating}</p>
-      <p>updateQueued: {updateQueued}</p> */}
-      {/* {props.stateProps?.lastMessage && <p>lastMessage: {props.stateProps?.lastMessage}</p>} */}
+  render () {
+    console.log('rendering...')
+    return <div className="widget-subscribe" style={{ overflow: 'auto', maxHeight: '700px' }}>
+        {this.state.extent}
+        {this.state.dataSourceId}
     </div>
-  )
+  }
 }
-
-interface ExtraProps {
-  lastMessage: string|undefined
-}
-
-// called everytime any widget updates shared store?
-// SubscriberDemo.mapExtraStateProps = (state: IMState, ownProps: AllWidgetProps<IMConfig>): ExtraProps => {
-// console.log('inside mapExtraStateProps')
-// let wId: string
-// // will trigger for 'lastMessage' property in ANY widget
-// for (const [key, value] of Object.entries(state.widgetsState)) {
-//   if (value.lastMessage) {
-//     wId = key
-//     console.log(`widget ${key}: `, value)
-//   }
-// }
-// if (wId) {
-//   return { lastMessage: state.widgetsState[wId]?.lastMessage }
-// } else {
-//   return { lastMessage: '' }
-// }
-// }
